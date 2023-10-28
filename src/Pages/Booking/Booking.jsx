4a830adb86ca5abece20d112b89d92eb32b1da01
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2023-10-29 02:21:58 +0600

    added my some changes

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -11,7 +11,53 @@ const Booking = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, [])
+    }, [url])
+
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure you want to delete')
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('Deleted successfully')
+                        const remainig = bookings.filter(booking => booking._id !== id)
+                        setBookings(remainig)
+                        console.log(remainig)
+                    }
+
+                })
+        }
+    }
+
+    const handleBookingConfirm = id => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'confirm' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.matchedCount > 0) {
+                    const remainig = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id == id);
+                    updated.status = 'confirm';
+                    const newBooking = [updated, ...remainig]
+                    setBookings(newBooking)
+
+                }
+
+            })
+    }
+
+
+
 
 
     return (
@@ -36,7 +82,13 @@ const Booking = () => {
                     <tbody>
 
                         {
-                            bookings.map(booking => <BookingRow key={booking._id} booking={booking}></BookingRow>)
+                            bookings.map(booking =>
+                                <BookingRow
+                                    key={booking._id}
+                                    booking={booking}
+                                    handleDelete={handleDelete}
+                                    handleBookingConfirm={handleBookingConfirm}
+                                ></BookingRow>)
                         }
 
 
--- src/Pages/Booking/BookingRow.jsx
+++ src/Pages/Booking/BookingRow.jsx
@@ -1,14 +1,16 @@
 
-const BookingRow = ({ booking }) => {
-    const { email, price, img, customer, service, date } = booking;
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+    const { _id, email, price, img, customer, service, status } = booking;
+
+
     return (
         <div>
             {/* row 1 */}
             <tr>
                 <th>
-                    <label>
-                        <input type="checkbox" className="checkbox" />
-                    </label>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
                 </th>
                 <td>
                     <div className="flex items-center space-x-3">
@@ -29,7 +31,11 @@ const BookingRow = ({ booking }) => {
                 </td>
                 <td>{price}</td>
                 <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
+                    {status === 'confirm' ?
+                        <span className="text-lg font-bold text-red-500">Confirm</span>
+                        :
+                        <button onClick={() => handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Confirm</button>
+                    }
                 </th>
             </tr>
         </div>
